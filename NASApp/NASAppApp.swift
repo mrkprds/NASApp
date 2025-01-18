3e7c88b6f765a691269946dd865ba440a847e6b8
Patrick Perdon <markpatrickperdon@gmail.com>
2025-01-18 16:27:26 +0800

    Initial Commit

---
+++ NASApp/NASAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAppApp.swift
+//  NASApp
+//
+//  Created by Patrick Perdon on 18/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NASAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
